c3c66dc95402f54aa1024f68c4ff9b4ecfafc889
X-s <xsmumu@gmail.com>
2014-10-30 14:39:59 +0800

    remove Camera Switch

--- gen/com/xs/stocksettings/R.java
+++ gen/com/xs/stocksettings/R.java
@@ -16,41 +16,31 @@ public final class R {
     public static final class string {
         public static final int about=0x7f040003;
         public static final int app_name=0x7f040000;
-        public static final int author=0x7f04001b;
-        public static final int author_xs=0x7f04001c;
-        public static final int author_xs_summary=0x7f04001d;
+        public static final int author=0x7f040015;
+        public static final int author_xs=0x7f040016;
+        public static final int author_xs_summary=0x7f040017;
         public static final int camera=0x7f040010;
         public static final int camera_sound=0x7f040007;
         public static final int camera_sound_summary_off=0x7f04000a;
         public static final int camera_sound_summary_on=0x7f04000b;
-        public static final int camera_switch=0x7f040011;
-        public static final int camera_switch_warning=0x7f040015;
-        public static final int camera_switch_warning_summary=0x7f040016;
-        public static final int cm_camera=0x7f040012;
-        public static final int cm_settings=0x7f040018;
+        public static final int cm_settings=0x7f040012;
         public static final int focus_sound=0x7f040009;
         public static final int focus_sound_summary_off=0x7f04000e;
         public static final int focus_sound_summary_on=0x7f04000f;
         public static final int mipop=0x7f040002;
-        public static final int miui_camera=0x7f040014;
         public static final int myappscreenmask=0x7f040001;
-        public static final int nubia_camera=0x7f040013;
-        public static final int proximity=0x7f04001e;
-        public static final int proximity_summaryOff=0x7f040020;
-        public static final int proximity_summaryOn=0x7f04001f;
         public static final int qq_group=0x7f040004;
         public static final int qq_group_summary=0x7f040005;
         public static final int reboot=0x7f040006;
         public static final int record_sound=0x7f040008;
         public static final int record_sound_summary_off=0x7f04000c;
         public static final int record_sound_summary_on=0x7f04000d;
-        public static final int system=0x7f040019;
-        public static final int tap_about=0x7f040017;
-        public static final int version=0x7f04001a;
+        public static final int system=0x7f040013;
+        public static final int tap_about=0x7f040011;
+        public static final int version=0x7f040014;
     }
     public static final class xml {
-        public static final int activity_camera_switch=0x7f030000;
-        public static final int activity_sound=0x7f030001;
-        public static final int activity_stocksettings=0x7f030002;
+        public static final int activity_sound=0x7f030000;
+        public static final int activity_stocksettings=0x7f030001;
     }
 }
--- src/com/xs/stocksettings/StockSettingsActivity.java
+++ src/com/xs/stocksettings/StockSettingsActivity.java
@@ -11,7 +11,6 @@ public class StockSettingsActivity extends PreferenceActivity {
 
 	private static final String ABOUT = "about";
 	private static final String REBOOT = "advanced_reboot";
-	private static final String PROXIMITY = "proximity_on_wake";
 
 	private static final String XS = SystemProperties
 			.get("ro.build.display.id");
@@ -21,7 +20,6 @@ public class StockSettingsActivity extends PreferenceActivity {
 
 	private Preference mAbout;
 	private CheckBoxPreference mReboot;
-	private CheckBoxPreference mProximity;
 
 	public void onCreate(Bundle SavedInstanceState) {
 		super.onCreate(SavedInstanceState);
@@ -29,12 +27,11 @@ public class StockSettingsActivity extends PreferenceActivity {
 
 		mAbout = (Preference) findPreference(ABOUT);
 		mReboot = (CheckBoxPreference) findPreference(REBOOT);
-		mProximity = (CheckBoxPreference) findPreference(PROXIMITY);
 	}
 
 	public void onStart() {
 		super.onStart();
-		if (!this.XS.equals("11-20140805-SNAPSHOT-M9-bacon-XS")
+		if (!this.XS.equals("11-20141008-SNAPSHOT-M11-bacon-XS")
 				|| !this.XS1.equals("weibo.com/acexs")
 				|| !this.XS2.equals("bacon_xs")) {
 			Toast.makeText(this, "您的ROM似乎不是正版ROM，请下载XS的正版作品！多谢支持！顺便鄙视盗版狗！",
@@ -55,14 +52,6 @@ public class StockSettingsActivity extends PreferenceActivity {
 				Settings.Secure.putInt(getContentResolver(), REBOOT, 1);
 			} else {
 				Settings.Secure.putInt(getContentResolver(), REBOOT, 0);
-			}
-		}
-
-		if (preference == mProximity) {
-			if (mProximity.isChecked()) {
-				Settings.System.putInt(getContentResolver(), PROXIMITY, 1);
-			} else {
-				Settings.System.putInt(getContentResolver(), PROXIMITY, 0);
 			}
 		}
 		return false;
